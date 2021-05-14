b8f281845c298328a5229ebf3bfaee7d4315bf02
cammie-myers <53240080+cammie-myers@users.noreply.github.com>
2021-05-13 19:25:42 -0700

    auto

--- src/main/java/frc/robot/auto_commands/SlalomAuto.java
+++ src/main/java/frc/robot/auto_commands/SlalomAuto.java
@@ -21,31 +21,14 @@ public class SlalomAuto extends SequentialCommandGroup{
 
     public SlalomAuto(){
       super(
-        new DriveTo(51),//forward
-        new Spin(90),
-        new DriveTo(60), //before long one
-        new Spin(-96),
-        new DriveTo(86),
-        new WaitCommand(0.2),
-        new DriveTo(90),//long run
-        new Spin(-90),
-        new DriveTo(54),// crosses sides
-        new Spin(90),
-        new DriveTo(64) , //heads towards driver station
-        new Spin(90),
-        new DriveTo(63), //turns to middle
-        new Spin(90),
-        new DriveTo(51), // goes down the middle
-        new Spin(90),
-        new DriveTo(66), //crosses
-        new Spin(-95),
-        new DriveTo(80), 
-        new Spin(-15),// long one
-        new DriveTo(83),
-        new Spin(-90),
-        new DriveTo(54),
-        new Spin(90),
-        new DriveTo(51)
+        new DriveTo(50),//forward
+        new DriveTo(50),
+        new DriveTo(20),
+        new Spin(180),//turn around
+        new DriveTo(50),//drive forward
+        new DriveTo(50),
+        new DriveTo(20),
+        new WaitCommand(0.2)
           ); 
     }
 }
